da261a10314a1928094bbc1bc03dcf4057a632c4
Peter Jones <peteflux@gmail.com>
2020-04-01 10:55:27 -0400

    created starter form in app.js and set up proper file structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends Component {
+
+  state = {
+    input: ""
+  };
+
+  handleChange = event => {
+    this.setState({
+      input: event.target.value
+    });
+  }
+
+  handleSubmit () {
+      alert("Form submitted!");
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <input 
+            type="text"
+            value={this.state.input}
+            placeholder="Type in a username here"
+            onChange={this.handleChange}
+            />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+        <p>Hello World</p>
+      </div>
   );
+}
 }
 
 export default App;
